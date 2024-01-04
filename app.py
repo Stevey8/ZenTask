868c7fd2f13d638214ce6dd6db41aa832adfd142
Steven Tsai <stevey@stevens-air.lan>
2024-01-03 18:58:10 -0800

    add due date and importance

--- app.py
+++ app.py
@@ -11,6 +11,7 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+import pandas as pd 
 
 app = Flask(__name__, static_url_path='/static')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -20,9 +21,11 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False) # nullable = True to make it 'blank'able
-    label = db.Column(db.String(50), nullable=True)
+    label = db.Column(db.String(50), nullable=False)
     completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
+    date_due = db.Column(db.DateTime, nullable=True)
+    importance = db.Column(db.String(50), nullable = False)
 
     def __repr__(self):
         return '<Task %r>' % self.id
@@ -34,20 +37,41 @@ def index():
     if request.method == 'POST':
         task_content = request.form['content']
         task_label = request.form['label']
-        new_task = Todo(content=task_content, label=task_label)
-        # new_task = Todo(content=task_label)
+        if request.form['date_due'] == '':
+            task_date_due = None
+        else:
+            task_date_due = datetime.strptime(request.form['date_due'], '%Y-%m-%d')
+        task_importance = request.form['importance']
+
+        new_task = Todo(content=task_content, label=task_label, date_due=task_date_due, importance = task_importance)
+        # new_task = Todo(content=task_content, label=task_label, date_due=task_date_due)
+
 
         try: 
             db.session.add(new_task)
             db.session.commit()
             return redirect('/')
-        except:
-            return 'there was an issue adding your task'
-
+        except Exception as e:
+            print(e)
+            return 'there was an issue adding your task:   ' + str(e)
         
     else:
         tasks = Todo.query.order_by(Todo.date_created).all()
         return render_template('task.html', tasks = tasks)
+    
+
+
+
+
+# create a dataframe 
+df = pd.DataFrame({'Name': ['Alice', 'Bob', 'Charlie'], 'Age': [25, 30, 35]})
+html_table = df.to_html(index=False)
+
+@app.route('/df')
+def show_df():
+    html_table = df.to_html(index=False)
+    return render_template('df.html', html_table=html_table)
+
 
 
 
@@ -78,7 +102,7 @@ def update(id):
     else:
         return render_template('update.html', task=task)
     
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
-
